733123d349b03b0df9a66087b6b3101230da83d9
ammarah mahmood <50270238+ammarahmahmood@users.noreply.github.com>
2019-05-22 01:46:29 +0500

    Add files via upload

---
+++ prime_num.py
@@ -0,0 +1,10 @@
+n = int(input("enter a number"))
+if n < 2:
+    print("is not prime")
+else:
+  for i in range(2, n):
+     if n % i == 0:
+        print("is not prime")
+        break
+  else:
+     print("is prime")
