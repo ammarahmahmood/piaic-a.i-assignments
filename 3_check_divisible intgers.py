f425d97bf2e0a24dc7e6dbacdc8d5da5f5704a35
ammarah mahmood <50270238+ammarahmahmood@users.noreply.github.com>
2019-05-22 15:58:08 +0500

    Add files via upload

---
+++ 3_check_divisible intgers.py
@@ -0,0 +1,6 @@
+n = int(input("_enter_numerater :" ))
+d = int(input("enter_denominator:"))
+if n % d == 0:
+  print("true")
+else:
+    print("false")
---
+++ 4_valu_of_(n+nn+nnn).py
@@ -0,0 +1,5 @@
+a = "n"
+b = "+nn"
+c = "+nnn"
+sum = (a + b + c)
+print (str(sum))
