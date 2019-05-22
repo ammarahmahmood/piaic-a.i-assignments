dd3d6226239094da065ef7ce169963f2661a3ebe
ammarah mahmood <50270238+ammarahmahmood@users.noreply.github.com>
2019-05-22 21:47:50 +0500

    Add files via upload

--- 5_checks_dates_2_days.py
+++ 5_checks_dates_2_days.py
@@ -1,5 +1,5 @@
 from datetime import datetime,  date
-d_1 = date(year = 2018, month = 7, day = 12)
+d_1 = date(year = 2018, month = 6, day = 6)
 d_2 = date(year =2019, month = 7, day = 12)
 days = d_2 - d_1
 print ("days:" , days)
